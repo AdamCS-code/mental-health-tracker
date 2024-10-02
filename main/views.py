5fa2cd76c46efef1a1831b0c94a6890294978f36
Adam <adamsembiring01@gmail.com>
2024-10-02 15:34:47 +0700

    Selesai tutorial 6

--- main/views.py
+++ main/views.py
@@ -9,6 +9,26 @@ from django.core import serializers
 from django.urls import reverse
 from main.forms import MoodEntryForm
 from main.models import MoodEntry
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
+
+@csrf_exempt
+@require_POST
+def add_mood_entry_ajax(request):
+    mood = strip_tags(request.POST.get("mood"))
+    feelings = strip_tags(request.POST.get("feelings"))
+    mood_intensity = request.POST.get("mood_intensity")
+    user = request.user
+
+    new_mood = MoodEntry(
+        mood=mood, feelings=feelings,
+        mood_intensity=mood_intensity,
+        user=user
+    )
+    new_mood.save()
+
+    return HttpResponse(b"CREATED", status=201)
 
 def edit_mood(request, id):
     # Get mood entry berdasarkan id
@@ -42,11 +62,11 @@ def show_json_by_id(request, id):
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_json(request):
-    data = MoodEntry.objects.all()
+    data = MoodEntry.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml(request):
-    data = MoodEntry.objects.all()
+    data = MoodEntry.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def create_mood_entry(request):
@@ -63,14 +83,12 @@ def create_mood_entry(request):
 
 @login_required(login_url='/login')
 def show_main(request):
-    mood_entries = MoodEntry.objects.filter(user=request.user)
 
     context = {
         'name': request.user.username,
         'name': 'Adam Caldipawell Sembiring',
         'class': 'PBP F',
         'npm': '2306227160',
-        'mood_entries': mood_entries,
         'last_login': request.COOKIES['last_login'],
     }
 
@@ -91,13 +109,14 @@ def register(request):
 def login_user(request):
    if request.method == 'POST':
       form = AuthenticationForm(data=request.POST)
-
       if form.is_valid():
             user = form.get_user()
             login(request, user)
             response = HttpResponseRedirect(reverse("main:show_main"))
             response.set_cookie('last_login', str(datetime.datetime.now()))
             return response
+      else:
+          messages.error(request, "Invalid username or password. Please try again.")
    else:
       form = AuthenticationForm(request)
    context = {'form': form}
